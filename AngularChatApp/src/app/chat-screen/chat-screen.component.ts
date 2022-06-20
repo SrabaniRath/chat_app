8e6454b18e7f1c58895a93d63ca581c78868b123
SrabaniRath <Srabani.Rath@nmbrsystems.com>
2022-06-20 13:43:38 +0530

    create new group with user participant,send message

--- AngularChatApp/src/app/chat-screen/chat-screen.component.ts
+++ AngularChatApp/src/app/chat-screen/chat-screen.component.ts
@@ -17,7 +17,8 @@ import {
   where,
 } from '@angular/fire/firestore';
 import { collection, deleteDoc, getDocs, writeBatch } from 'firebase/firestore';
-import { concatMap, map, Observable } from 'rxjs';
+import { combineLatest, concatMap, map, Observable, startWith } from 'rxjs';
+
 @Component({
   selector: 'app-chat-screen',
   templateUrl: './chat-screen.component.html',
@@ -34,29 +35,15 @@ export class GroupMessagesComponent implements OnInit {
   chatHistory: any = [];
   newGroupName: any = '';
   userLists: any = [];
-  // chatGroups: any = [
-  //   'oc-group-1',
-  //   'oc-group-3'
-  // ];
   chatGroups: any = [];
+  searchControl:any='';
   constructor(private messageService: MessageService, private fireStore: Firestore) { }
 
   ngOnInit(): void {
 
-    //  let inbox = sessionStorage['inbox'];
-    //  if(inbox && typeof inbox == 'string'){
-    //   inbox = JSON.parse(inbox);
-    //  }
-
-    //   if(!inbox){
-    //     sessionStorage.setItem('inbox',JSON.stringify(chatData))
-    //     this.chatInbox = chatData;
-    //     console.log({ chatData: this.chatInbox })
-    //   }else{
-    //     this.chatInbox=inbox;
-    //   }
     this.getmyGroups();
     this.getAllUsers();
+    //this.createChat();
   }
   /**
    * 
@@ -64,39 +51,46 @@ export class GroupMessagesComponent implements OnInit {
    */
   setSelectGroup(e: any) {
     this.selectedGroupName = e;
-    console.log(this.selectedGroupName);
-    //this.loadChatHistory(e)
     this.getChatMessages(e);
   }
 
   sendMessage() {
-    this.updateMessages(this.msgToSend);
+    let timestamp = Date.now();
+    //this.updateMessages(this.msgToSend);
+    const ref = collection(this.fireStore, 'oc-group-messages',`${this.selectedGroupName}`, 'messages');
+    addDoc(ref, {
+      sentAt:timestamp,
+      messageText:this.msgToSend,
+      sentBy:this.loggedInUser
+    })
     this.msgToSend = '';
   }
+/**
+ * @ not required
+ */
+  // updateMessages(msg: any) {
 
-  updateMessages(msg: any) {
-
-    const updatedInbox = this.chatInbox.map((eachInbox: any) => {
-      if (eachInbox.name == this.selectedChat.name) {
-        const chats = eachInbox.chats;
-        return {
-          ...eachInbox,
-          chats: [...chats, {
-            sender: this.loggedInUser,
-            content: msg,
-            id: eachInbox.chats.length + 1
-          }]
-        }
-      }
-      return eachInbox
-    })
-    sessionStorage.setItem('inbox', JSON.stringify(updatedInbox))
-    this.chatInbox = updatedInbox;
-    this.selectedChat = updatedInbox.find((eachI: any) => {
-      return eachI.name == this.selectedChat.name;
-    })
-    console.log(updatedInbox);
-  }
+  //   const updatedInbox = this.chatInbox.map((eachInbox: any) => {
+  //     if (eachInbox.name == this.selectedChat.name) {
+  //       const chats = eachInbox.chats;
+  //       return {
+  //         ...eachInbox,
+  //         chats: [...chats, {
+  //           sender: this.loggedInUser,
+  //           content: msg,
+  //           id: eachInbox.chats.length + 1
+  //         }]
+  //       }
+  //     }
+  //     return eachInbox
+  //   })
+  //   sessionStorage.setItem('inbox', JSON.stringify(updatedInbox))
+  //   this.chatInbox = updatedInbox;
+  //   this.selectedChat = updatedInbox.find((eachI: any) => {
+  //     return eachI.name == this.selectedChat.name;
+  //   })
+  //   //console.log(updatedInbox);
+  // }
 
   /**
    * 
@@ -107,7 +101,7 @@ export class GroupMessagesComponent implements OnInit {
      * @Usage user lists
      */
     // this.firestore.collection('oc-group').valueChanges().subscribe((res)=>{
-    //   console.log("user lists", res);
+    //   //console.log("user lists", res);
     // })
 
     /**
@@ -115,7 +109,7 @@ export class GroupMessagesComponent implements OnInit {
      */
     // this.chatHistory = [];
     // this.firestore.collection(`oc-group-messages/${groupName}/messages`).valueChanges().subscribe((res: any) => {
-    //   console.log("messages lists", res);
+    //   //console.log("messages lists", res);
     //   this.chatHistory = res || [];
     //  // this.chatHistory.sort((a: any, b: any) => a.sentAt - b.sentAt);
     //   // this.chatHistory = this.chatHistory.map((eachMsg: any) => {
@@ -124,7 +118,7 @@ export class GroupMessagesComponent implements OnInit {
     //   //     sentAt: new Date(eachMsg.sentAt)
     //   //   }
     //   // })
-    //   console.log(this.chatHistory)
+    //   //console.log(this.chatHistory)
     // })
 
   }
@@ -139,8 +133,9 @@ export class GroupMessagesComponent implements OnInit {
     const queryAll = query(ref, orderBy('sentAt', 'asc'));
     const data = collectionData(queryAll)
     data.forEach(e => {
-      //console.log(e)
+      console.log(e)
       this.chatHistory = e;
+      console.log(this.chatHistory)
     });
   }
 
@@ -157,25 +152,21 @@ export class GroupMessagesComponent implements OnInit {
     data.subscribe(console.log)
     data.pipe(map((rec: any) => rec.map((eachGrp: any) => eachGrp['name']))).subscribe((res: any) => {
       this.chatGroups = res || [];
-      console.log(res);
+      //console.log(res);
     })
-    // data.forEach(e => {
-    //   console.log(e)
-    //   this.chatGroups = e.map((eachGrpName) => {
-    //     return eachGrpName['name']
-    //   });
-    // })
   }
 
   /**
    * create a chat group
    */
-  createChat() {
-    const ref = collection(this.fireStore, 'oc-group-messages', 'oc-group-2', 'messages');
-    addDoc(ref, {
-      name: 'oc-grp'
-    })
-  }
+  // createChat(user:any) {
+  //   console.log(user);
+  //   const ref = collection(this.fireStore, 'oc-group-messages',`${ this.chatGroups}`, 'messages');
+  //   addDoc(ref, {
+  //     name: 'oc-grp'
+  //   })
+  //   console.log("create chat",ref);
+  // }
   /**
    * fetching all user lists
    */
@@ -183,28 +174,41 @@ export class GroupMessagesComponent implements OnInit {
     const ref = collection(this.fireStore, 'oc-user');
     const querryAll = query(ref);
     const data = collectionData(querryAll);
+    data.subscribe(console.log)
     data.pipe(
       map((userData:any)=>userData.map((rec:any)=>rec['uid']))
     ).subscribe((res:any)=>{
       this.userLists=res || [];
        console.log(res);
     })
-    // data.forEach(e => {
-    //   console.log("user listss", e)
-    //   this.userLists = e.map((eachUser) => {
-    //     return eachUser['uid']
-    //   });
 
-    // })
-    //console.log("user listss", this.userLists)
+  }
+  userSelects:any=[];
+  show: boolean = false;
+  suggest() {
+    this.show = true;
   }
 
+  isSelected(user:any) {
+   return this.userSelects.findIndex((item:any) => item === user) > -1 ? true : false;
+  }
+ 
+  selectUser(user:any) {
+    this.userSelects.find((item:any) => item === user) ? (this.userSelects = this.userSelects.filter((item:any) => item !== user)) :this.userSelects.push(user);
+    this.show=false;
+  }
+
+  deleteSelects(user:any) {
+    this.userSelects = this.userSelects.filter((item:any) => item!== user);
+  
+  }
   /**
    * create a new group
    */
   addNewGroup() {
+    //console.log(this.userSelects);
     let timestamp = Date.now();
-    let userArray= this.userLists;
+    let userArray=this.userSelects;
     const ref = collection(this.fireStore, 'oc-group');
     const refdoc = doc(ref, this.newGroupName)  //username and timstamp
     setDoc(refdoc, {
--- AngularChatApp/src/app/message.service.ts
+++ AngularChatApp/src/app/message.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { collection, collectionData, Firestore, query } from '@angular/fire/firestore';
+import { map } from 'rxjs';
 // import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 
@@ -6,12 +8,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  userLists:any=[];
+  constructor(private firestore:Firestore) { }
 
-  constructor() { }
+  // getAllUsers() {
+  //   const ref = collection(this.firestore, 'oc-user');
+  //   const querryAll = query(ref);
+  //   return collectionData(querryAll);
+  //   // data.subscribe(console.log)
+  //   // data.pipe(
+  //   //   map((userData:any)=>userData.map((rec:any)=>rec['displayname']))
+  //   // ).subscribe((res:any)=>{
+  //   //   this.userLists=res || [];
+  //   //    //console.log(res);
+  //   // })
 
-
-  getMessageLists() { 
-     //return this.firestore.collection('oc-group-messages').doc('oc-group-1').collection('message').snapshotChanges();
-    //return this.firestore.collection('oc-group-messages').snapshotChanges();
-  }
+  // }
 }
